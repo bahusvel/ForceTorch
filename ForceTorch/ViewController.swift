927fa5f0f4c6129fe6bc3f78073930e0058a186c
Denis Lavrov <bahus.vel@gmail.com>
2016-01-23 22:10:18 +1300

    Initial Commit

---
+++ ForceTorch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForceTorch
+//
+//  Created by denis lavrov on 23/01/16.
+//  Copyright © 2016 Denis Lavrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
