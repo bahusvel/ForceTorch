b2fe4c9a468e02c52b26d277bb61b10ffba3b54c
Denis Lavrov <bahus.vel@gmail.com>
2016-02-15 15:41:15 +1300

    Initializing repo

--- ForceTorch/ViewController.swift
+++ ForceTorch/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
+    let camera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first where traitCollection.forceTouchCapability == .Available {
+            do{
+                try camera.lockForConfiguration()
+                try camera.setTorchModeOnWithLevel(Float(touch.force / touch.maximumPossibleForce))
+                camera.unlockForConfiguration()
+            } catch {
+                print("Error could not change the flashlight brightness")
+            }
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        do{
+            try camera.lockForConfiguration()
+            camera.torchMode = .Off
+            camera.unlockForConfiguration()
+        } catch {
+            print("Error could not change the flashlight brightness")
+        }
+    }
 
 }
 
